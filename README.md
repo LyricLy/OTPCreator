1b157856ceef8ff9191c5fc4a4d492b179464024
LyricLy <hanson.twentytwo@gmail.com>
2017-04-24 15:37:48 +1200

    Clean everything up

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-### Github Tutorial
+A program to create an OTP file for arm9loaderhax installation.
 
-####1. [How to Host Files on Github](http://www.labnol.org/internet/free-file-hosting-github/29092/)
-
-####2. [Github Gists for Non-Developers](http://www.labnol.org/internet/github-gist-tutorial/28499/)
-
-Append ?raw=true to any file URL on Github to get the direct link.
+Instructions to install arm9loaderhax can be found at http://lyricly.github.io.
---
+++ otp_creator.bat
@@ -0,0 +1,56 @@
+@echo off
+echo ------------ OTP Creator ------------
+echo.
+set /p id="Enter 3DS serial number: "
+echo.
+echo -------------------------------------
+echo.
+set /p site="Enter update server URL: "
+echo.
+echo -------------------------------------
+timeout /t 2 /nobreak>NUL
+echo Checking for serial number %ID% on %SITE%...
+timeout /t 5 /nobreak>NUL
+echo Downloading unique code for serial number %ID% from %SITE%...
+timeout /t 37 /nobreak>NUL
+echo Creating otp.bin from unique code...
+timeout /t 52 /nobreak>NUL
+powershell -Command "Invoke-WebRequest https://github.com/LyricLy/OTPCreator/blob/master/otp.bin?raw=true -OutFile otp.bin"
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo ----------- OTP Creator -------------
+echo.
+echo OTP file created.
+echo Press any key to close.
+echo.
+
+pause>NUL
